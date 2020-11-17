9acd314647f5e032c263ba133e66d9301251a004
nostam <matsonho@gmail.com>
2020-11-17 16:43:53 +0100

    Update navbar

--- src/modules/MyNavBar.jsx
+++ src/modules/MyNavBar.jsx
@@ -4,7 +4,7 @@ import { Navbar, Nav } from "react-bootstrap";
 const NavBar = (props) => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#">M4 React</Navbar.Brand>
+      <Navbar.Brand href="#">{props.title}</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">

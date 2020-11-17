2a5a0dd6b3357b50b4fc6900482e9dceec57a9e0
nostam <matsonho@gmail.com>
2020-11-17 16:43:30 +0100

    update Jumbotron

--- src/modules/MyJumbotron.jsx
+++ src/modules/MyJumbotron.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Jumbotron, Button } from "react-bootstrap";
+import { Jumbotron, Button, Container } from "react-bootstrap";
 
 const MyJumbotron = (props) => {
   return (
-    <Jumbotron>
-      <h1>Hello, MyJumbotron!</h1>
-      <p>This should be a non-fluid jumbotron.</p>
-      {/* <p> */}
-      <Button variant="primary">Learn more</Button>
-      {/* </p> */}
-    </Jumbotron>
+    <Container>
+      <Jumbotron className="mt-5">
+        <h1>Hello, Strangers!</h1>
+        <p>Pick a book you like!</p>
+        <hr className="my-4" />
+
+        <p>...but do you read books?</p>
+        <Button variant="primary">Learn more</Button>
+      </Jumbotron>
+    </Container>
   );
 };
 export default MyJumbotron;
